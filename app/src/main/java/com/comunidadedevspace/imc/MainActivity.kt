39b6e03bd0db41ce96c943b1205e7726a71d98fa
miguelalvaradoferreira <miguelalvaradofer@gmail.com>
2024-12-01 15:38:55 +0000

    mostrar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,29 +1,23 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import android.icu.text.TimeZoneFormat.TimeType
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Recuperar os componentes EditText
-        // Criar uma variavel e associar o componente de UI<EditText>
-        // Recuperar o botão
 
         val edtpeso  = findViewById<TextInputEditText>(R.id.edittext_peso)
         val edtaltura = findViewById<TextInputEditText>(R.id.edittext_altura)
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
 
-
         btnCalcular.setOnClickListener {
-
 
             val pesoSTR: String= edtpeso.text.toString()
             val alturaSTR: String = edtaltura.text.toString()
@@ -39,10 +33,15 @@ class MainActivity : AppCompatActivity() {
 
            } else {
                 val peso = pesoSTR.toFloat()
-                val altura alturaSTR.toFloat()
+                val altura = alturaSTR.toFloat()
 
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
                 println("Ação do botão " + resultado)
 
             }
